9cb64ca8da65c938eddaee5133a496fc2fa55eac
treshia <117747401+floofycato@users.noreply.github.com>
2024-01-30 21:51:41 +0800

    Import products from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 
 let productsHTML = '';
 
@@ -7,7 +8,7 @@ products.forEach((product) => {
   <div class="product-container">
     <div class="product-image-container">
       <img class="product-image"
-        src="${product.image}">
+        src="${product.image}"> 
     </div>
 
     <div class="product-name limit-text-to-2-lines">
